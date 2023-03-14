ea71a24a092ccb6df71cc0d9653d3ccafd73edeb
Abraham Hosch <hosch250@live.com>
2023-03-13 23:29:47 -0500

    Tweaks

--- FDR/Core/Joshs_Template_GameDefines.sql
+++ FDR/Core/Joshs_Template_GameDefines.sql
@@ -58,7 +58,29 @@ VALUES	('LEADER_HOSCH_LEADER_CUSTOM',		'LEADER_LEADER_CUSTOM_BACKGROUND',		'LEAD
 ----------------------------------------------------------------------------------------------------------------------------	
 INSERT INTO CivilizationLeaders	
 		(CivilizationType,			LeaderType,							CapitalName)
-VALUES	('CIVILIZATION_AMERICA',	'LEADER_HOSCH_LEADER_CUSTOM',		'LOC_CITY_NAME_HOSCH_CIV_CUSTOM_1');
+VALUES	('CIVILIZATION_AMERICA',	'LEADER_HOSCH_LEADER_CUSTOM',		'LOC_CITY_NAME_WASHINGTON');
+
+
+-- todo: I get started on the coast more consistently without this???
+-- INSERT INTO StartBiasTerrains
+-- 		(CivilizationType,			TerrainType,		Tier)
+-- VALUES	('CIVILIZATION_AMERICA',	'TERRAIN_COAST',	1);
+
+-- todo: this doesn't seem to work...
+-- INSERT INTO CivilopediaPageChapterParagraphs
+-- 		(SectionId,			PageId, 						ChapterId,	Paragraph,																		SortIndex)
+-- VALUES	('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_1',		1),
+-- 		('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_2',		2),
+-- 		('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_3',		3),
+-- 		('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_4',		4),
+-- 		('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_5',		5),
+-- 		('CIVILIZATIONS',	'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_6',		6),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_1',		1),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_2',		2),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_3',		3),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_4',		4),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_5',		5),
+-- 		('LEADERS',			'LEADER_HOSCH_LEADER_CUSTOM',	'HISTORY',	'LOC_PEDIA_LEADERS_PAGE_LEADER_HOSCH_LEADER_CUSTOM_CHAPTER_HISTORY_PARA_6',		6);
 
 
 --------------------------------------------------------------------------------------------------------------------------		
@@ -70,36 +92,24 @@ VALUES	('LEADER_HOSCH_LEADER_CUSTOM',		'ART_LEADER_LEADER_CUSTOM.dds');
 
 
 
+--==========================================================================================================================
+-- LEADERS: TRAITS
+--==========================================================================================================================
+-- Types
+--------------------------------------------------------------------------------------------------------------------------	
+INSERT INTO Types	
+		(Type,														Kind)
+VALUES	('TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL',						'KIND_TRAIT'),
+		('TRAIT_LEADER_HOSCH_LEADER_CCC',							'KIND_TRAIT');
+--------------------------------------------------------------------------------------------------------------------------			
+-- Traits			
+--------------------------------------------------------------------------------------------------------------------------				
+INSERT INTO Traits				
+		(TraitType,													Name,													Description)
+VALUES	('TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL',						'LOC_TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL_NAME',			'LOC_TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL_DESCRIPTION'),
+		('TRAIT_LEADER_HOSCH_LEADER_CCC',							'LOC_TRAIT_LEADER_HOSCH_LEADER_CCC_NAME',				'LOC_TRAIT_LEADER_HOSCH_LEADER_CCC_DESCRIPTION');
+-- todo: remove one of these; have one trait and one agenda
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- --==========================================================================================================================
--- -- LEADERS: TRAITS
--- --==========================================================================================================================
--- -- Types
--- --------------------------------------------------------------------------------------------------------------------------	
--- INSERT INTO Types	
--- 		(Type,														Kind)
--- VALUES	('TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL',						'KIND_TRAIT');	
--- --------------------------------------------------------------------------------------------------------------------------			
--- -- Traits			
--- --------------------------------------------------------------------------------------------------------------------------				
--- INSERT INTO Traits				
--- 		(TraitType,													Name,													Description)
--- VALUES	('TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL',						'LOC_TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL_NAME',			'LOC_TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL_DESCRIPTION');	
 -- --------------------------------------------------------------------------------------------------------------------------		
 -- -- TraitModifiers		
 -- --------------------------------------------------------------------------------------------------------------------------			
@@ -120,95 +130,15 @@ VALUES	('LEADER_HOSCH_LEADER_CUSTOM',		'ART_LEADER_LEADER_CUSTOM.dds');
 -- VALUES	('HOSCH_LEADER_NEW_DEAL_EXTRA_SLOT_1',						'GovernmentSlotType',			'SLOT_ECONOMIC');
 
 
-
-
-
-
-
-
-
-
-
-
-
--- --==========================================================================================================================
--- -- CIVILIZATIONS: TRAITS
--- --==========================================================================================================================
--- -- Types
--- --------------------------------------------------------------------------------------------------------------------------	
--- INSERT INTO Types	
--- 		(Type,													Kind)
--- VALUES	('TRAIT_CIVILIZATION_HOSCH_CCC',						'KIND_TRAIT'),
--- 		('TRAIT_CIVILIZATION_BUILDING_HOSCH_CUSTOM',			'KIND_TRAIT');	
--- --------------------------------------------------------------------------------------------------------------------------			
--- -- Traits			
--- --------------------------------------------------------------------------------------------------------------------------				
--- INSERT INTO Traits				
--- 		(TraitType,												Name,													Description)
--- VALUES	('TRAIT_CIVILIZATION_HOSCH_CCC',						'LOC_TRAIT_CIVILIZATION_HOSCH_CCC',						'LOC_TRAIT_CIVILIZATION_HOSCH_CCC_DESCRIPTION'),
--- 		('TRAIT_CIVILIZATION_BUILDING_HOSCH_CUSTOM',			'LOC_TRAIT_CIVILIZATION_BUILDING_HOSCH_CUSTOM_NAME',	'LOC_TRAIT_CIVILIZATION_BUILDING_HOSCH_CUSTOM_DESCRIPTION');	
--- --------------------------------------------------------------------------------------------------------------------------		
--- -- TraitModifiers		
--- --------------------------------------------------------------------------------------------------------------------------			
--- INSERT INTO TraitModifiers			
--- 		(TraitType,											ModifierId)
--- VALUES	('TRAIT_CIVILIZATION_HOSCH_CCC',					'HOSCH_TRAIT_EXP_ATK'),
--- 		('TRAIT_CIVILIZATION_HOSCH_CCC',					'HOSCH_TRAIT_EXP'),
--- 		('TRAIT_CIVILIZATION_HOSCH_CCC',					'HOSCH_TRAIT_GoldPerKill');																					
--- --------------------------------------------------------------------------------------------------------------------------
--- -- Modifiers
--- --------------------------------------------------------------------------------------------------------------------------
--- INSERT INTO Modifiers	
--- 		(ModifierId,												ModifierType)
--- VALUES	('HOSCH_TRAIT_EXP_ATK',										'MODIFIER_PLAYER_UNITS_ADJUST_UNIT_ATTACK_EXPERIENCE_MODIFIER'),
--- 		('HOSCH_TRAIT_EXP',											'MODIFIER_PLAYER_UNITS_ADJUST_UNIT_EXPERIENCE_MODIFIER'),
--- 		('HOSCH_TRAIT_GoldPerKill',									'MODIFIER_PLAYER_UNITS_ADJUST_POST_COMBAT_YIELD');			
--- --------------------------------------------------------------------------------------------------------------------------
--- -- ModifierArguments
--- --------------------------------------------------------------------------------------------------------------------------
--- INSERT INTO ModifierArguments
--- 		(ModifierId,												Name,						Value)
--- VALUES	('HOSCH_TRAIT_EXP_ATK',										'Amount',					'50'),
--- 		('HOSCH_TRAIT_EXP',											'Amount',					'50'),
--- 		('HOSCH_TRAIT_GoldPerKill',									'YieldType',				'YIELD_GOLD'),
--- 		('HOSCH_TRAIT_GoldPerKill',									'PercentDefeatedStrength',	'100');				
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 --------------------------------------------------------------------------------------------------------------------------
 -- LeaderTraits
 --------------------------------------------------------------------------------------------------------------------------	
 INSERT INTO LeaderTraits	
 		(LeaderType,					TraitType)
 VALUES	('LEADER_HOSCH_LEADER_CUSTOM',	'TRAIT_LEADER_HOSCH_LEADER_NEW_DEAL'),
-		('LEADER_HOSCH_LEADER_CUSTOM',	'TRAIT_CIVILIZATION_HOSCH_CCC');
+		('LEADER_HOSCH_LEADER_CUSTOM',	'TRAIT_LEADER_HOSCH_CCC');
+-- todo: remove one of these; have one trait and one agenda
 
 
-
--- ----------------------------------------------------------------------------------------------------------------------------
--- -- CivilizationTraits
--- ----------------------------------------------------------------------------------------------------------------------------	
--- INSERT INTO CivilizationTraits	
--- 		(TraitType,															CivilizationType)
--- VALUES	('TRAIT_CIVILIZATION_HOSCH_CCC',									'CIVILIZATION_AMERICA');
-
-
-
-
-
-
-
+-- todo: insert "new deal" or "CCC" into agendas
+-- <Row AgendaType="AGENDA_BULL_MOOSE" Name="LOC_AGENDA_BULL_MOOSE_NAME" Description="LOC_AGENDA_BULL_MOOSE_DESCRIPTION"/>
